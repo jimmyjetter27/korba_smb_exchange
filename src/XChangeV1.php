833a9334075bdabee126f2f37f2f90ca54c49e98
jimmyjetter27 <jimmyjetter27@gmail.com>
2021-10-18 16:31:19 +0000

    altered foreach

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -467,8 +467,6 @@ class XChangeV1 extends API
         $result = $this->call('get_mtndata_product_id/', []);
         Log::debug(json_encode($result));
         Log::debug($result);
-        $bundles = $result['bundles'][0]['bundles'];
-        dd($bundles);
         $list = [];
         if (isset($result['success']) && $result['success']) {
             foreach ($result['bundles'][0]['bundles'] as $bundle) {
