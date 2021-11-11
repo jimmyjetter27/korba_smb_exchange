6aa1eb1bcc5bff5335cd8171d89413b5959cae75
jimmyjetter27 <jimmyjetter27@gmail.com>
2021-11-11 11:02:51 +0000

    altered sms

--- src/SMS.php
+++ src/SMS.php
@@ -3,8 +3,6 @@
 
 namespace Korba;
 
-use Illuminate\Support\Facades\Log;
-
 /**
  * Class SMS help send messages.
  * Class to make use of Info SMS API service to send sms messages. It extends Class API. {@inheritDoc}
@@ -36,14 +34,7 @@ class SMS extends API
         parent::__construct($base_url, $headers);
     }
 
-    /**
-     * SMS public function send.
-     * It is used to send SMS
-     * @param string $text Body of the SMS
-     * @param string|array $to Number of the recipient
-     * @param string|null $from Name of the send. if null will fallback to global_from
-     * @return bool|string
-     */
+
     public function send($text, $to, $from = null)
     {
         $formatter = function ($value) {
@@ -55,38 +46,6 @@ class SMS extends API
             'text' => $text
         ];
         $data['from'] = $from == null ? $this->global_from : $from;
-//        return $this->call('/send_sms/', $data);
-        return $this->sending($to, $text);
-    }
-
-    public function sending($to, $text)
-    {
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => env('SMS_BASE_URL'),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS =>'{
-    "phone_number": "'.$to.'",
-    "message":"'.$text.'",
-    "sender_id":"KorbaSMB"
-}',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Token '.env('SMS_AUTH_TOKEN'),
-                'Content-Type: application/json'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-//        echo $response;
-        Log::debug($response);
+        return $this->call('/send_sms/', $data);
     }
 }
