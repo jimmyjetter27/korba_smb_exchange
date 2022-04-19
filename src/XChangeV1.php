d3c199f4079fc4ac383279be899f6240c3381ab4
jimmyjetter27 <jimmyjetter27@gmail.com>
2022-04-19 16:04:13 +0000

    is ussd added

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -82,7 +82,8 @@ class XChangeV1 extends API
 
     public function collect(
         $customer_number, $amount, $transaction_id, $network_code, $callback_url,
-        $vodafone_voucher_code = null, $description = null, $payer_name = null, $extra_info = null)
+        $vodafone_voucher_code = null, $description = null, $payer_name = null, $extra_info = null,
+        $is_ussd = true)
     {
         $data = [
             'customer_number' => Util::numberGHFormat($customer_number),
@@ -96,6 +97,7 @@ class XChangeV1 extends API
             'description' => $description,
             'payer_name' => $payer_name,
             'extra_info' => $extra_info,
+            'is_ussd' => $is_ussd
         ];
 
         $this->add_optional_data($data, $opt_data);
