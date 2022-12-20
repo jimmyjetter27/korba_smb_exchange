2464588713c94b185033e354ae4dbcaa4535bb28
jimmyjetter27 <jimmyjetter27@gmail.com>
2022-12-20 14:36:31 +0000

    add

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -25,8 +25,9 @@ class XChangeV1 extends API
     protected $secret_key;
     protected $client_key;
     protected $client_id;
+    protected $xchange_url;
 //    protected static $live_url = 'https://xchange.korbaweb.com/api/v1.0';
-    protected static $aws_url = 'http://internal-awseb-e-e-awsebloa-kxexw3t2bgt7-1521297916.eu-west-1.elb.amazonaws.com/api/v1.0';
+//    protected static $aws_url = 'http://internal-awseb-e-e-awsebloa-kxexw3t2bgt7-1521297916.eu-west-1.elb.amazonaws.com/api/v1.0';
 
     public function __construct($secret_key, $client_key, $client_id, $mode = 'test', $xchange_url = null, $proxy = null)
     {
