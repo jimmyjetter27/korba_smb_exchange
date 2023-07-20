d72af32e50142bf0cd0d36d7836abeeb00e83911
jimmyjetter27 <jimmyjetter27@gmail.com>
2023-07-20 13:26:24 +0000

    added getHmacValue function

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -62,6 +62,22 @@ class XChangeV1 extends API
         return ["Authorization: HMAC {$this->client_key}:{$hmac_signature}"];
     }
 
+
+    private function getHmacValue($data)
+    {
+        $data = (gettype($data) == 'string') ? json_decode($data, true) : $data;
+        $data = array_merge($data, ['client_id' => $this->client_id]);
+        $message = '';
+        $i = 0;
+        ksort($data);
+        foreach ($data as $key => $value) {
+            $message .= ($i == 0) ? "{$key}={$value}" : "&{$key}={$value}";
+            $i++;
+        }
+        $hmac_signature = hash_hmac('sha256', $message, $this->secret_key);
+        return "{$this->client_key}:{$hmac_signature}";
+    }
+
     protected function call($endpoint, $data, $extra_headers = null, $timeout = 20, $connection_timeout = 5)
     {
         $data = array_merge($data, ['client_id' => $this->client_id]);
@@ -1279,7 +1295,7 @@ class XChangeV1 extends API
             'card_id' => $card_id ?? ''
         ];
         return parent::brassica_exchange(
-            'ecg_vending_prepaid_inquiry/', $data, 'post', $this->getHMACHeader($data), $this->client_id);
+            'ecg_vending_prepaid_inquiry/', $data, 'post', $this->getHmacValue($data), $this->client_id);
 //        return $this->call('ecg_vending_prepaid_inquiry/', $data);
     }
 
